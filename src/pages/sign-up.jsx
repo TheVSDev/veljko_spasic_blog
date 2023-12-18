6dda9f461f2929c33c049b09695f0f656b04fa0a
TheVSDev <vspasic03@gmail.com>
2023-12-18 09:22:32 +0100

    refactor: sign up cleanup

--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,16 +1,17 @@
+// Imports
 import { object } from "yup"
+import { useRouter } from "next/router"
+import { useMutation } from "@tanstack/react-query"
+
 import Container from "@/web/components/Container"
 import Form from "@/web/components/Form"
 import FormField from "@/web/components/FormField"
-import ClickableRedirect from "@/web/components/ClickableRedirect"
 import SubmitButton from "@/web/components/SubmitButton"
 import {
   nameValidator,
   emailValidator,
   passwordValidator
 } from "@/utils/validators"
-import { useRouter } from "next/router"
-import { useMutation } from "@tanstack/react-query"
 import apiClient from "@/web/services/apiClient"
 
 const initialValues = {
@@ -28,7 +29,7 @@ const validationSchema = object({
 const SignUpPage = () => {
   const router = useRouter()
   const { isSuccess, mutateAsync } = useMutation({
-    mutationFn: (values) => apiClient.post("/users", values),
+    mutationFn: (values) => apiClient.post("/users", values)
   })
   const handleSubmit = async (values) => {
     await mutateAsync(values)
@@ -40,41 +41,41 @@ const SignUpPage = () => {
     router.push("/sign-in")
   }
 
-  return(
-  <Container className="w-[450px] h-[589px] ml-[35%] mt-[3%]">
-    <Form
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-      title="Sign Up">
-      <FormField
-        name="firstName"
-        type="text"
-        placeholder="John"
-        label="First name"
-      />
-      <FormField
-        name="lastName"
-        type="text"
-        placeholder="Doe"
-        label="Last name"
-      />
-      <FormField
-        name="email"
-        type="email"
-        placeholder="johndoe@popoo.com"
-        label="E-mail"
-      />
-      <FormField
-        name="password"
-        type="password"
-        placeholder="C4gdv@03tw_"
-        label="Password"
-      />
-      <SubmitButton btnLabel="Create account" onSubmit={handleSubmit} />
-      <ClickableRedirect redirectMessage="You already have an account ?" redirectLink="/sign-in" redirectLinkLabel="Sign in" />
-    </Form>
-  </Container>)
+  return (
+    <Container className="w-[450px] h-[569px] ml-[35%] mt-[3%]">
+      <Form
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        title="Sign Up">
+        <FormField
+          name="firstName"
+          type="text"
+          placeholder="John"
+          label="First name"
+        />
+        <FormField
+          name="lastName"
+          type="text"
+          placeholder="Doe"
+          label="Last name"
+        />
+        <FormField
+          name="email"
+          type="email"
+          placeholder="johndoe@popoo.com"
+          label="E-mail"
+        />
+        <FormField
+          name="password"
+          type="password"
+          placeholder="C4gdv@03tw_"
+          label="Password"
+        />
+        <SubmitButton btnLabel="Create account" onSubmit={handleSubmit} />
+      </Form>
+    </Container>
+  )
 }
 
 export default SignUpPage
