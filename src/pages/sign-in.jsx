347fbf57d348d224fd1d6b06a6fe30b769f058af
TheVSDev <vspasic03@gmail.com>
2023-12-18 14:02:59 +0100

    feat: alert messages components

--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -1,16 +1,16 @@
+// Imports
 import { object } from "yup"
 import { useRouter } from "next/router"
 import { useMutation } from "@tanstack/react-query"
+
 import Container from "@/web/components/Container"
 import Form from "@/web/components/Form"
 import FormField from "@/web/components/FormField"
 import SubmitButton from "@/web/components/SubmitButton"
-import {
-  emailValidator,
-  passwordValidator
-} from "@/utils/validators"
+import { emailValidator, passwordValidator } from "@/utils/validators"
 import apiClient from "@/web/services/apiClient"
 import { useSession } from "@/web/components/SessionContext"
+import ErrorMessage from "@/web/components/AlertMessages/ErrorMessage"
 
 const initialValues = {
   email: "",
@@ -23,8 +23,8 @@ const validationSchema = object({
 const SignInPage = () => {
   const router = useRouter()
   const { saveSessionToken } = useSession()
-  const { mutateAsync } = useMutation({
-    mutationFn: (values) => apiClient.post("/sessions", values),
+  const { mutateAsync, error } = useMutation({
+    mutationFn: (values) => apiClient.post("/sessions", values)
   })
   const handleSubmit = async (values) => {
     const { result: jwt } = await mutateAsync(values)
@@ -35,27 +35,29 @@ const SignInPage = () => {
   }
 
   return (
-  <Container className="w-[450px] h-[366px] ml-[35%] mt-[5%]">
-    <Form
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-      title="Sign In">
-      <FormField
-        name="email"
-        type="email"
-        placeholder="johndoe@popoo.com"
-        label="E-mail"
-      />
-      <FormField
-        name="password"
-        type="password"
-        placeholder="C4gdv@03tw_"
-        label="Password"
-      />
-      <SubmitButton btnLabel="Log in" onSubmit={handleSubmit} />
-    </Form>
-  </Container>)
+    <Container className="w-[450px] h-[366px] ml-[35%] mt-[5%]">
+      <ErrorMessage error={error} />
+      <Form
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        title="Sign In">
+        <FormField
+          name="email"
+          type="email"
+          placeholder="johndoe@popoo.com"
+          label="E-mail"
+        />
+        <FormField
+          name="password"
+          type="password"
+          placeholder="C4gdv@03tw_"
+          label="Password"
+        />
+        <SubmitButton btnLabel="Log in" onSubmit={handleSubmit} />
+      </Form>
+    </Container>
+  )
 }
 
 export default SignInPage
--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,19 +1,22 @@
 // Imports
 import { object } from "yup"
-import { useRouter } from "next/router"
 import { useMutation } from "@tanstack/react-query"
+import Link from "next/link"
 
 import Container from "@/web/components/Container"
 import Form from "@/web/components/Form"
 import FormField from "@/web/components/FormField"
 import SubmitButton from "@/web/components/SubmitButton"
+import Button from "@/web/components/Button"
 import {
   nameValidator,
   emailValidator,
   passwordValidator
 } from "@/utils/validators"
 import apiClient from "@/web/services/apiClient"
+import SuccessMessage from "@/web/components/AlertMessages/SuccessMessage"
 
+// Form functions
 const initialValues = {
   firstName: "",
   lastName: "",
@@ -26,8 +29,17 @@ const validationSchema = object({
   email: emailValidator.label("E-mail"),
   password: passwordValidator.label("Password")
 })
+// Success message component to be shown if sign up is successful
+const Success = () => (
+  <Container className="w-[600px] ml-[30%] mt-[4%] border-none flex flex-col gap-7">
+    <SuccessMessage>Account created successfully</SuccessMessage>
+    <Link href="/sign-in">
+      <Button btnLabel="Log in your account" />
+    </Link>
+  </Container>
+)
+// Sign up page
 const SignUpPage = () => {
-  const router = useRouter()
   const { isSuccess, mutateAsync } = useMutation({
     mutationFn: (values) => apiClient.post("/users", values)
   })
@@ -38,7 +50,7 @@ const SignUpPage = () => {
   }
 
   if (isSuccess) {
-    router.push("/sign-in")
+    return <Success />
   }
 
   return (
