e917f5873b0d68f37845e2b3acb6c40bdad34bef
TheVSDev <vspasic03@gmail.com>
2024-01-06 16:31:10 +0100

    feat: variant buttons

--- src/pages/create-post.jsx
+++ src/pages/create-post.jsx
@@ -1,10 +1,15 @@
 import Title from "@/web/components/Title"
 import Editor from "@/web/components/Editor"
+import Button from "@/web/components/Button"
 
 const CreatePostPage = () => (
   <>
     <Title titleLabel="Create a post" />
     <Editor />
+    <div className="flex flex-row justify-center gap-7">
+      <Button btnLabel="Save" />
+      <Button btnLabel="Cancel" variant="secondary" />
+    </div>
   </>
 )
 
