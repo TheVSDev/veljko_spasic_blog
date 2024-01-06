3abf01ed7d35d1fef758069dd40c31d8b8a99ca5
TheVSDev <vspasic03@gmail.com>
2024-01-06 16:34:56 +0100

    chore: cancel create post

--- src/pages/create-post.jsx
+++ src/pages/create-post.jsx
@@ -1,16 +1,24 @@
 import Title from "@/web/components/Title"
 import Editor from "@/web/components/Editor"
 import Button from "@/web/components/Button"
+import { useRouter } from "next/router"
 
-const CreatePostPage = () => (
-  <>
-    <Title titleLabel="Create a post" />
-    <Editor />
-    <div className="flex flex-row justify-center gap-7">
-      <Button btnLabel="Save" />
-      <Button btnLabel="Cancel" variant="secondary" />
-    </div>
-  </>
-)
+const CreatePostPage = () => {
+  const router = useRouter()
+  const handleCancel = () => {
+   router.push("/my-posts")
+ }
+  
+  return (
+    <>
+      <Title titleLabel="Create a post" />
+      <Editor />
+      <div className="flex flex-row justify-center gap-7">
+        <Button btnLabel="Save" />
+        <Button btnLabel="Cancel" variant="secondary" onClick={handleCancel} />
+      </div>
+    </>
+  )
+}
 
 export default CreatePostPage
