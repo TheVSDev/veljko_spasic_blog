a32eeb05258746a328ff52c3a177c5d77cfea6ad
TheVSDev <vspasic03@gmail.com>
2024-01-27 17:49:22 +0100

    chore: post seeds to populate db

---
+++ src/db/seeds/posts.mjs
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker"
+
+export const seed = async (db) => {
+  await db("posts").insert(
+    [...Array(20)].map(() => ({
+      postTitle: faker.lorem.words(),
+      postBody: faker.lorem.paragraphs(),
+    }))
+  )
+}
