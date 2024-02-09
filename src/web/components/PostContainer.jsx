bbb7715cdc9e9d76f109811f7c7e4e6a3f37ad14
TheVSDev <vspasic03@gmail.com>
2024-02-09 20:56:05 +0100

    feat: posts timestamp

--- src/web/components/PostContainer.jsx
+++ src/web/components/PostContainer.jsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import { useState } from "react"
+import { formatDateTimeShort } from "@/utils/formatters"
 import CommentContainer from "@/web/components/CommentContainer"
 import Icon from "@/web/components/Icon"
 
@@ -18,7 +19,7 @@ const PostContainer = (props) => {
 
   return (
     <div className="w-full mt-10 grid place-items-center">
-      {posts.map(({ postTitle, postBody }, index) => (
+      {posts.map(({ postTitle, postBody, createdAt }, index) => (
         <div
           key={postTitle}
           className="mt-10 border-2 border-black rounded-lg w-[1000px] shadow-lg">
@@ -26,6 +27,9 @@ const PostContainer = (props) => {
             {postTitle}
           </span>
           <p className="p-2">{postBody}</p>
+          <p className="p-2 float-right text-stone-500 italic text-base">
+            Created at: {formatDateTimeShort(new Date(createdAt))}
+          </p>
           <div className="flex items-center">
             <button onClick={() => toggleCommentSection(index)}>
               <Icon icon="comment" className="w-[30px]" />
