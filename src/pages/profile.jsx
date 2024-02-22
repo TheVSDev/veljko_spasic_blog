0f5960aee45e406d951e19f96d7bd9b782c0abc2
TheVSDev <vspasic03@gmail.com>
2024-02-23 00:58:15 +0100

    chore: profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,57 +1,19 @@
 import { useSession } from "@/web/components/SessionContext"
-import { useQuery } from "@tanstack/react-query"
-import apiClient from "@/web/services/apiClient"
 import Title from "@/web/components/ui/Title"
-import Loader from "@/web/components/ui/Loader"
-import ErrorMessage from "@/web/components/ui/alert-messages/ErrorMessage"
+import Container from "@/web/components/ui/containers/Container"
+import AccountTypeTag from "@/web/components/ui/AccountTypeTag"
 
 const Profile = () => {
   const { session } = useSession()
-  const {
-    data: user,
-    isLoading,
-    isError
-  } = useQuery({
-    queryKey: ["profile"],
-    queryFn: async () => {
-      try {
-        if (!session || !session.id) {
-          throw new Error("User information not available.")
-        }
-
-        const userData = await apiClient.get(`/users/${session.userId}`)
-
-        return userData.result
-      } catch (error) {
-        throw new Error("User information not available.")
-      }
-    },
-    enabled: Boolean(session)
-  })
-
-  if (isLoading) {
-    return <Loader />
-  }
 
   return (
     <div>
       <Title titleLabel="Profile" />
-      {isError ? (
-        <ErrorMessage error="User information not available." />
-      ) : (
-        <>
-          {user ? (
-            <>
-              <p>First Name: {user.firstName}</p>
-              <p>Last Name: {user.lastName}</p>
-              <p>E-mail: {user.email}</p>
-              <p>Account Type: {user.userType}</p>
-            </>
-          ) : (
-            <ErrorMessage error="User information not available." />
-          )}
-        </>
-      )}
+        <Container className="mx-auto w-[50%] mt-[100px]">
+              <h2 className="text-center font-bold text-3xl mb-5">#{session?.id} <span className="text-[#0da839]">{session?.firstName} {session?.lastName}</span></h2>
+              <p>E-mail: {session?.email}</p>
+              <p>Account type: <AccountTypeTag accountTypeLabel={session?.userType} /></p>
+        </Container>
     </div>
   )
 }
