bc2d1040ab5f6f99740c34fb25ecbb11419286b1
TheVSDev <vspasic03@gmail.com>
2024-02-23 14:03:36 +0100

    fix: format profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -9,11 +9,18 @@ const Profile = () => {
   return (
     <div>
       <Title titleLabel="Profile" />
-        <Container className="mx-auto w-[50%] mt-[100px]">
-              <h2 className="text-center font-bold text-3xl mb-5">#{session?.id} <span className="text-[#0da839]">{session?.firstName} {session?.lastName}</span></h2>
-              <p>E-mail: {session?.email}</p>
-              <p>Account type: <AccountTypeTag accountTypeLabel={session?.userType} /></p>
-        </Container>
+      <Container className="mx-auto w-[50%] mt-[100px]">
+        <h2 className="text-center font-bold text-3xl mb-5">
+          #{session?.id}{" "}
+          <span className="text-[#0da839]">
+            {session?.firstName} {session?.lastName}
+          </span>
+        </h2>
+        <p>E-mail: {session?.email}</p>
+        <p>
+          Account type: <AccountTypeTag accountTypeLabel={session?.userType} />
+        </p>
+      </Container>
     </div>
   )
 }
